4e1aec1df6e7047f521bfc73eb1346a4461bd387
Liwei Lyu <119458223+Liwei-Lyu@users.noreply.github.com>
2025-07-15 20:54:41 +0200

    Source_code_20250627.R

--- Source_code_20250627.R
+++ Source_code_20250627.R
@@ -1,94 +1,150 @@
-# Load required packages (consolidated list)
-library(tidyverse)
-library(ggplot2)
-library(ggpubr)
-library(rstatix)
 library(vegan)
-library(phyloseq)
-library(Maaslin2)
-library(ComplexHeatmap)
-library(circlize)
-library(patchwork)
-library(ggrepel)
-library(openxlsx)
-library(lme4)
-library(emmeans)
-library(performance)
+library(tidyverse)
 
-# Data loading ----
-input.name <- "phanta"
-obser.name <- "_bac"
-obser.fullname <- "_bacterial species"
-out_dir <- "/Users/nhx573/Library/CloudStorage/OneDrive-UniversityofCopenhagen/work_in_2020-2023/DIRECT_and_stability_studyfrom20230206/results/community_analysis/"
-working_dir <- "/Users/nhx573/Library/CloudStorage/OneDrive-UniversityofCopenhagen/work_in_2020-2023/DIRECT_and_stability_studyfrom20230206/processed_data/phanta1.1.0/DIRECT_phanta"
-setwd(working_dir)
+meta <- read.delim("metadata.txt", sep="\t", row.names=1)
 
-# Load metadata and marker data
-marker_df <- read.delim("biomarkers/marker_df.txt", sep = "\t", row.names = 1)
-meta <- read.delim("metadata/meta_20240719.txt", sep = ",", row.names = 1)
-meta_adv <- meta[, !colnames(meta) %in% c("Anthropometric", "OGTT", "MGS")]
+baseline_counts <- read.delim("baseline_species_counts.txt")
+endline_counts <- read.delim("endline_species_counts.txt")
 
-# Get paired samples
-paired_subject.ID <- unique(meta[meta$Timepoint == "Endline" & meta$SequencingID != "unknown", "SubjectID"])
-paired_sample.ID <- meta[meta$SubjectID %in% paired_subject.ID, "SampleID"]
+feature_table <- merge(baseline_counts, endline_counts, by="Taxon") 
+rownames(feature_table) <- feature_table$Taxon
+feature_table$Taxon <- NULL
+feature_table <- t(feature_table)
 
-# Load additional metadata
-meta_all <- readRDS("metadata/meta_all20240803.R")
-meta_all_wide <- readRDS("metadata/meta_all_wide20240803.R")
-meta_adv_short <- readRDS("results/community_analysis/meta_adv_short.rds")
-meta_adv_short_short <- unique(meta_adv_short[, c("SubjectID", "Group")])
+min_reads <- min(rowSums(feature_table))
+rarefied_data <- rrarefy(feature_table, sample=min_reads)
 
-# Process lifestyle data
-lifestyle <- read.delim2("metadata/Lifestyle_score.txt")
-lifestyle_bas <- lifestyle %>% 
-  mutate(SubjectID = SampleID,
-         SampleID = paste0(SampleID, "_bas")) %>% 
-  select("SampleID", "Smoking.Status.BL.n", "Value.vm.hpf.mean.BL.Med.n", "HDI.Med.n", "LifestyleScore", "SubjectID")
+taxa_meta <- data.frame(
+  feature_ID = paste0("feature_", 1:ncol(rarefied_data)),
+  feature_name = colnames(rarefied_data)
+)
 
-lifestyle_end <- lifestyle_bas %>% 
-  mutate(SampleID = paste0(SubjectID, "_end"))
-lifestyle_all <- rbind(lifestyle_bas, lifestyle_end)
-rownames(lifestyle_all) <- lifestyle_all$SampleID
+extract_rank <- function(string, rank) {
+  str_extract(string, paste0(rank, "_[^|]*")) %>%
+    str_replace(paste0(rank, "_"), "")
+}
 
-# Merge lifestyle data with metadata
-lifestyle_all <- lifestyle_all[meta_adv_short$SampleID,]
-meta_adv_short <- merge(meta_adv_short, lifestyle_all[, c("SampleID", "Smoking.Status.BL.n", "Value.vm.hpf.mean.BL.Med.n", "HDI.Med.n", "LifestyleScore")], by = "SampleID")
+taxa_meta <- taxa_meta %>%
+  mutate(
+    kingdom = ifelse(grepl("Bacteria", feature_name), "Bacteria",
+                     ifelse(grepl("Viruses", feature_name), "Bacteriophage", "Fungi")),
+    phylum = extract_rank(feature_name, "phylum"),
+    genus = extract_rank(feature_name, "genus"),
+    species = extract_rank(feature_name, "species")
+  )
 
-# Define sample groups
-T2D_ids <- meta_adv_short[meta_adv_short$Group == "T2D", "SampleID"]
-IGR_ids <- meta_adv_short[meta_adv_short$Group == "IGR", "SampleID"]
-NGR_ids <- meta_adv_short[meta_adv_short$Group == "NGR", "SampleID"]
-nk_ids <- meta_adv_short[meta_adv_short$Group == "nk", "SampleID"]
+calculate_diversity <- function(count_matrix) {
+  data.frame(
+    SampleID = rownames(count_matrix),
+    Richness = specnumber(count_matrix),
+    Shannon = diversity(count_matrix, index="shannon"),
+    Simpson = diversity(count_matrix, index="simpson")
+  )
+}
 
-T2D_subjectids <- unique(meta_adv_short[meta_adv_short$Group == "T2D", "SubjectID"])
-IGR_subjectids <- unique(meta_adv_short[meta_adv_short$Group == "IGR", "SubjectID"])
-NGR_subjectids <- unique(meta_adv_short[meta_adv_short$Group == "NGR", "SubjectID"])
+diversity_df <- calculate_diversity(rarefied_data)
 
-# Load feature tables
-baseline_feature.table <- read.delim("Phanta_baseline_outputs/counts_species.txt", check.names = FALSE)
-endline_feature.table <- read.delim("Phanta_followup/counts_species.txt", check.names = FALSE)
-baseline.totalreads <- read.delim("Phanta_baseline_outputs/total_reads.tsv", check.names = FALSE)
-endline.totalreads <- read.delim("Phanta_followup/total_reads.tsv", check.names = FALSE)
-totalreads <- rbind(baseline.totalreads, endline.totalreads)
+diversity_meta <- merge(diversity_df, meta, by="SampleID")
 
-# Process feature tables
-baseline_feature.table$Taxon_Lineage_with_IDs <- NULL
-endline_feature.table$Taxon_Lineage_with_IDs <- NULL
+plot_diversity <- function(data, metric) {
+  ggplot(data, aes(x=Timepoint, y=.data[[metric]], color=Group)) +
+    geom_boxplot() +
+    geom_point(aes(group=SubjectID)) +
+    labs(y=metric, title=paste(metric, "Diversity"))
+}
 
-feature.table <- merge(baseline_feature.table, endline_feature.table, by = "Taxon_Lineage_with_Names")
-rownames(feature.table) <- feature.table$Taxon_Lineage_with_Names
-feature.table$Taxon_Lineage_with_Names <- NULL
-feature.table <- as.data.frame(t(feature.table))
+plot_diversity(diversity_meta, "Shannon")
+plot_diversity(diversity_meta, "Simpson")
 
-# Match sample IDs with metadata
-feature.table <- feature.table[intersect(rownames(feature.table), meta$SequencingID),]
-lookup <- setNames(meta$SampleID, meta$SequencingID)
-rownames(feature.table) <- lookup[rownames(feature.table)]
-feature.table <- feature.table[paired_sample.ID,]
+calculate_prevalence <- function(count_matrix, timepoint) {
+  presence <- count_matrix > 0
+  colMeans(presence) * 100
+}
 
-# Define color schemes
-color.scheme <- c("nk" = "#BF9E91", "IGR" = "#BF826B", "NGR" = "#73564C")
-color.scheme.tp <- c("Endline" = "#CB8D8B", "Baseline" = "#E1BD83")
-color.scheme.tpgp <- c("nk_Endline" = "black", "IGR_Endline" = "blue", "NGR_Endline" = "#038C3E",
-                       "T2D_Endline" = "red", "nk_Baseline" = "grey", "IGR_Baseline" = "dodgerblue",
-                       "NGR_Baseline" = "green", "T2D_Baseline" = "orange")
+baseline_prevalence <- calculate_prevalence(rarefied_data[grep("baseline", rownames(rarefied_data)),])
+endline_prevalence <- calculate_prevalence(rarefied_data[grep("endline", rownames(rarefied_data)),])
+
+prevalence_df <- data.frame(
+  feature_ID = names(baseline_prevalence),
+  Baseline = baseline_prevalence,
+  Endline = endline_prevalence,
+  Change = endline_prevalence - baseline_prevalence
+) %>%
+  merge(taxa_meta, by="feature_ID")
+
+calculate_relative_abundance <- function(count_matrix) {
+  total_counts <- rowSums(count_matrix)
+  sweep(count_matrix, 1, total_counts, FUN="/")
+}
+
+relative_abundance <- calculate_relative_abundance(feature_table)
+
+analyze_feature_subsets <- function(rel_abundance_matrix, feature_sets) {
+  results <- list()
+  
+  for(set_name in names(feature_sets)) {
+    subset_matrix <- rel_abundance_matrix[, feature_sets[[set_name]]]
+    
+    results[[set_name]] <- list(
+      summary_stats = summary(rowSums(subset_matrix, na.rm=TRUE)),
+      taxa_names = taxa_metadata[colnames(subset_matrix), "taxa_name"]
+    )
+  }
+  
+  return(results)
+}
+
+feature_sets <- list(
+  core_union = union_features,
+  core_shared = shared_features,
+  prevalent = prevalent_features
+)
+
+subset_results <- analyze_feature_subsets(relative_abundance, feature_sets)
+
+subset_results$core_union$summary_stats
+subset_results$core_union$taxa_names
+
+
+
+distance_matrix <- vegdist(relative_abundance_data, method = "bray") %>% as.matrix()
+
+dist_all <- function(distance) {
+  diag(distance) <- NA
+  sample_ids <- unique(gsub("_(bas|end)$", "", rownames(distance)))
+  bas_index <- paste(sample_ids, "_bas", sep = "")
+  end_index <- paste(sample_ids, "_end", sep = "")
+  intra_distances <- diag(distance[bas_index, end_index])
+  inter_distances_bas <- sapply(seq_along(bas_index), function(i) {
+    median(distance[bas_index[i], bas_index[-i]], na.rm = TRUE)
+  })
+  inter_distances_end <- sapply(seq_along(end_index), function(i) {
+    median(distance[end_index[i], end_index[-i]], na.rm = TRUE)
+  })
+  data.frame(SubjectID = sample_ids, intra_distances, inter_distances_bas, inter_distances_end)
+}
+
+all_distance <- dist_all(distance_matrix)
+
+intra_group_dissimilarity <- sapply(unique(metadata$SubjectID), function(individual) {
+  individual_samples <- which(metadata$SubjectID == individual)
+  individual_group <- metadata$Group[individual_samples][1]
+  other_samples <- which(metadata$Group == individual_group & !metadata$SubjectID %in% individual)
+  inter_dissimilarities <- c(as.matrix(distance_matrix)[individual_samples, other_samples])
+  data.frame(SubjectID = individual, intra_group_dissimilarity = median(inter_dissimilarities, na.rm = TRUE))
+}, simplify = FALSE) %>% do.call(rbind, .)
+
+all_distance <- merge(all_distance, intra_group_dissimilarity, by = "SubjectID")
+all_distance$DMI <- (all_distance$inter_distances_bas + all_distance$inter_distances_end) / 2 - all_distance$intra_distances
+
+merged_data <- merge(all_distance, metadata, by = "SubjectID")
+
+long_data <- pivot_longer(merged_data,
+                          cols = c("intra_distances", "inter_distances_bas", "inter_distances_end", "intra_group_dissimilarity"),
+                          names_to = "Type", values_to = "Value") %>%
+  mutate(Type = factor(Type, levels = c("intra_distances", "inter_distances_bas", "inter_distances_end", "intra_group_dissimilarity")))
+
+p_val_table <- long_data %>%
+  wilcox_test(Value ~ Type, paired = TRUE, exact = TRUE) %>%
+  mutate(FDR = p.adjust(p, method = "BH"),
+         signif_label = case_when(FDR < 0.001 ~ "***", FDR < 0.01 ~ "**", FDR < 0.05 ~ "*", FDR < 0.1 ~ "ˆ", TRUE ~ "ns"))
